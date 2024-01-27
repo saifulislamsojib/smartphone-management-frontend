4f0c9c71314e4f863c98931eb1ac6c8de59ddcbd
saifulislamsojib <saifulislam.sojib56@gmail.com>
2024-01-28 03:45:47 +0600

    [feat] base url added in env

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: import.meta.env.VITE_API_BASE_URL,
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("jwt-token");
       if (token) {
